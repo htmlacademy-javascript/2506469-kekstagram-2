e433416b7632eefcda75f158c762c333a51b4b6f
Никита Шеленков <di.kava@yandex.ru>
2024-12-03 23:19:31 +0300

    исправляю ошибки после проверки

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,11 @@
-// eslint-disable-next-line no-unused-vars
+
 function checkLengthString(string = '', length = 1) {
   return (string <= length);
 }
 
-// eslint-disable-next-line no-unused-vars
+console.log(checkLengthString('тестовая строка', 5));
+
+
 function findPolydrome(string = '') {
   string = string.replaceAll(' ', '').toLowerCase();
 
@@ -15,38 +17,37 @@ function findPolydrome(string = '') {
   return string === reversedLength;
 }
 
+console.log('\n--- Тесты для findPolydrome ---');
+console.log(`Строка является палиндромом: ${findPolydrome('топот') === true}`);
+console.log(`Строка является палиндромом с разным регистром: ${findPolydrome('ДовОд') === true}`);
+console.log(`Строка не является палиндромом: ${findPolydrome('Кекс') === false}`);
+console.log(`Строка является палиндромом: ${findPolydrome('Лёша на полке клопа нашёл ') === true}`);
+
 // Функция принимает строку,
 // извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
 
 function extractsNumbers(string = '') {
 
-  let str = '';
-  const newStr = string.toString();
+  let result = '';
+  string = string.toString();
 
-  for (let i = 0; i <= newStr.length - 1; i++) {
-    if (parseInt(newStr[i], 10) || parseInt(newStr[i], 10) === 0) {
-      str += newStr[i];
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (parseInt(string[i], 10) || parseInt(string[i], 10) === 0) {
+      result += string[i];
     }
   }
 
-  if (str) {
-    const num = Number(str);
-    return Math.abs(num);
+  if (result) {
+    return Number(result);
   } else {
     return NaN;
   }
 
 }
 
-// eslint-disable-next-line no-console
 console.log(extractsNumbers('2023 год'));
-// eslint-disable-next-line no-console
 console.log(extractsNumbers('ECMAScript 2022'));
-// eslint-disable-next-line no-console
 console.log(extractsNumbers('1 кефир, 0.5 батона'));
-// eslint-disable-next-line no-console
 console.log(extractsNumbers('агент 007'));
-// eslint-disable-next-line no-console
 console.log(extractsNumbers('а я томат'));
-// eslint-disable-next-line no-console
 console.log(extractsNumbers(-1));
