7882b99dbccb79a0af1a47c132953478b1a9006e
Никита Шеленков <di.kava@yandex.ru>
2024-12-15 23:02:51 +0300

    модуляция

---
+++ js/util.js
@@ -0,0 +1,16 @@
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const getId = () => {
+  let id = 1;
+  function increasesId () {
+    return id++;
+  }
+  return increasesId;
+}
+
+export {getRandomInt};
+export {getId};
